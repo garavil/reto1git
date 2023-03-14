19459b5b4d2ba151f6099398550b9408b032a508
Gara Vilches <garavilches@gmail.com>
2023-03-14 15:37:13 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function suma (a,b){
+    resultado = a + b
+    console.log(resultado);
+}
+
+function resta (a,b){
+    resultado = a - b
+    console.log(resultado);
+}
+
+function mult (a,b){
+    resultado = a * b
+    console.log(resultado);
+}
+
+function div (a,b){
+    resultado = a / b
+    console.log(resultado);
+}
+function cuadrado(a){
+    resultado = a * a
+    console.log(resultado)
+}
+
+module.exports = {suma, resta, mult, div, cuadrado}
